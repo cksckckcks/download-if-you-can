c93144c695bc08baefa6330abb695336d8e129aa
cksckckcks <chan679999@naver.com>
2025-11-10 15:32:31 +0900

    feat: MainScreen 시스템 바 고려 패딩 추가

--- composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/MainScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
@@ -49,6 +52,7 @@ fun MainScreen() {
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.systemBars)
     ) {
 
         Column(
