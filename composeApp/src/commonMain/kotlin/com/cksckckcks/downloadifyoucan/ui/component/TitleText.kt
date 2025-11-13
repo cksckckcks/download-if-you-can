3737477f4bdc66226dac989967140d6fb88ee86a
cksckckcks <chan679999@naver.com>
2025-11-14 01:37:52 +0900

    feat: 타이틀 관련 Text 컴포넌트 작성

---
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/component/TitleText.kt
@@ -0,0 +1,20 @@
+package com.cksckckcks.downloadifyoucan.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.cksckckcks.downloadifyoucan.theme.pretendard
+
+
+@Composable
+fun TitleText(
+    text: String
+) {
+    Text(
+        text = text,
+        fontFamily = pretendard(),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
