3ca4ce1bf763cbfab9d080ab66a2963ff45b26b7
cksckckcks <chan679999@naver.com>
2025-11-18 23:51:43 +0900

    feat: 클래스 중복 문제 해결을 위한 Priority 클래스 구조 변경

--- composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/component/PrioritySelector.kt
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/component/PrioritySelector.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.cksckckcks.downloadifyoucan.model.Priority
+import com.cksckckcks.downloadifyoucan.model.image
 import com.cksckckcks.downloadifyoucan.theme.MainColor
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
