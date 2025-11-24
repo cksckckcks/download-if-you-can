67ea24fb925a895da147b04d31a96665f4071150
cksckckcks <chan679999@naver.com>
2025-11-24 20:57:28 +0900

    feat: 공용 스플래시 스크린 작성 및 기존 안드로이드 스플래시 기능 off

--- composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/App.kt
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/App.kt
@@ -3,10 +3,23 @@ package com.cksckckcks.downloadifyoucan
 import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
 import com.cksckckcks.downloadifyoucan.ui.screen.MainScreen
+import com.cksckckcks.downloadifyoucan.ui.screen.SplashScreen
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    Navigator(MainScreen())
+    var showSplash by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(2000) // 2초
+        showSplash = false
+    }
+
+    if (showSplash) {
+        SplashScreen()
+    } else {
+        Navigator(MainScreen())
+    }
 }
