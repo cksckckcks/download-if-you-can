e6b4153265996d3312e507815c81b1b408c823da
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-11-24 19:31:40 +0900

    feat: 기본 배경 색상 화이트로 변경 (#25)

--- composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/AddToDoScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/AddToDoScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -66,16 +67,16 @@ fun AddToDoScreenContent(
     val itemSpaceValue = 21.dp
 
 
-    Scaffold(
+        Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .windowInsetsPadding(WindowInsets.systemBars)
-            .padding(horizontal = 24.dp)
+            .windowInsetsPadding(WindowInsets.systemBars),
+            containerColor = Color.White
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 22.dp)
+                .padding(top = 22.dp, start = 24.dp, end = 24.dp)
                 .verticalScroll(rememberScrollState())
         ) {
             TitleText(text = "할일 추가")
