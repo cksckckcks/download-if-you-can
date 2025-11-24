d08e1c71245156251a085dbffd2c8021a33a40cc
cksckckcks <chan679999@naver.com>
2025-11-24 15:39:21 +0900

    refactor: 필요 없는 import 제거

--- composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/cksckckcks/downloadifyoucan/ui/screen/MainScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
